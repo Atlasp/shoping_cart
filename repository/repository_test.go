3714054dc45fcdbaa386010847d1928bfe5e6331
simaspaulikas <simaspaulikas@yahoo.com>
2022-04-20 17:35:09 +0300

    remove hosts

--- repository/repository_test.go
+++ repository/repository_test.go
@@ -17,6 +17,7 @@ func CreateTestingRepository() Repository {
 		Discount:          1,
 		FreeItemThreshold: 5,
 	}
+
 	dsn := "host=localhost user=revel password=revel dbname=revel_test port=5433 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
